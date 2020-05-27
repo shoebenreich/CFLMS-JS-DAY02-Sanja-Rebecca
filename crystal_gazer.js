86aba51d54ccb85b65e08a368ed88bf9ce38ab73
Sanja Höbenreich <sanja.hoebenreich@outlook.at>
2020-05-27 09:39:20 +0200

    uploaded first basic exercise

---
+++ crystal_gazer.js
@@ -0,0 +1,5 @@
+function crystalGazer(numOfChildren, partner, location, job) {
+	document.write(`You will be a ${job} in ${location} and married to ${partner} with ${numOfChildren} children.`);
+}
+
+crystalGazer(4, 'Berta', 'Kanada', 'Chef');
